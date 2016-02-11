52d5ed790c95fec7b22e833b9b388ff12dc893fd
Mathieu Wauters <mwauters@deloitte.com>
2016-02-11 20:51:20 +0100

    Finalied part 1

--- part1.R
+++ part1.R
@@ -1,10 +1,11 @@
 ####################################
 #   Part 1 - Parameters & settings
 ####################################
+library(ggplot2)
 nbr_simulations<-1000
 nbr_exponentials<-40
 lambda<-0.2
-set.seed(1234)
+set.seed(2534)
 
 ####################################
 #   Part 2 - Data Generation
@@ -28,11 +29,15 @@ print(1/(lambda^2))
 ####################################
 #   Part 4 - Normality check
 ####################################
-std_values<-(apply(value_matrix,1,mean)-mean(apply(value_matrix,1,mean)))/sd(apply(value_matrix,1,mean))
-std_values<-as.data.frame(std_values)
-colnames(std_values)[1]<-"z"
-ggplot(std_values, aes(x=z))+geom_histogram(aes(y=..density..),binwidth=.3,colour="black", fill="grey",alpha=.2)+stat_function(fun=dnorm)+geom_density(alpha=.3, fill="blue")+geom_vline(xintercept=0,col="red")+theme_bw()
-shapiro.test(std_values$z)$p.value
+
+mu<-1/lambda
+sigma<-1/lambda
+tmp<-as.data.frame(tmp)
+colnames(tmp)<-"x"
+ggplot(tmp, aes(x=x))+geom_histogram(aes(y=..density..),binwidth=.05,colour="black", fill="grey",alpha=.2)+stat_function(geom = "line", fun = dnorm, arg = list(mean = mu, sd = sigma/sqrt(nbr_exponentials)),
+              size = 1, colour = "red")+geom_density(alpha=.3, fill="blue")+theme_bw()
+shapiro.test(tmp$x)$p.value
+
 ####################################
 #   Part 5 - Coverage
 ####################################
